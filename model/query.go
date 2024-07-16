b4a6b57fbaf82a32f81e1484c363e29689256921
expertep <esred@hotmail.com>
2024-07-17 01:30:52 +0700

    delete not use

--- model/query.go
+++ model/query.go
@@ -9,18 +9,3 @@ type Query struct {
 	Name   string   `form:"name"`
 	Range  []string `form:"range[]"`
 }
-
-type QueryTime struct {
-	CreateAt string   `form:"create_at"`
-	Range    []string `form:"range[]"`
-	Sort     string   `form:"sort"`
-	Order    int      `form:"order"`
-	Page     int64    `form:"page" default:20`
-	Limit    int64    `form:"limit" default:10`
-}
-type QueryS3 struct {
-	Limit                 int64  `form:"limit" default:10`
-	Sort                  string `form:"sort"`
-	Order                 int    `form:"order"`
-	NextContinuationToken string `form:"next_continuation_token"`
-}
