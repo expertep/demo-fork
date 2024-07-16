fb21d6b4d2514e9ad24ccd74bf78b6a32d65a63a
expertep <esred@hotmail.com>
2024-07-17 00:48:26 +0700

    remove comment on cors function

--- app.go
+++ app.go
@@ -21,8 +21,7 @@ func StartServer() {
 }
 
 func CORS(c *gin.Context) {
-	// First, we add the headers with need to enable CORS
-	// Make sure to adjust these headers to your needs
+
 	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Methods", "*")
 	c.Header("Access-Control-Allow-Headers", "*")
@@ -30,19 +29,10 @@ func CORS(c *gin.Context) {
 	c.Header("Access-Control-Allow-Credential", "true")
 	c.Header("Content-Type", "application/json")
 
-	// Second, we handle the OPTIONS problem
 	if c.Request.Method != "OPTIONS" {
 		c.Next()
 		return
 	} else {
-		// Everytime we receive an OPTIONS request,
-		// we just return an HTTP 200 Status Code
-		// Like this, Angular can now do the real
-		// request using any other method than OPTIONS
-		// c.AbortWithStatusJSON(401, gin.H{
-		// 	"code":    "ERROR",
-		// 	"message": "Unauthorized",
-		// })
 		c.AbortWithStatus(http.StatusOK)
 		return
 	}
