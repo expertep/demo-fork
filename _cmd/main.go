b3a21fa5d1e05761784b8d02b082061fe594e664
expertep <esred@hotmail.com>
2024-07-17 00:45:49 +0700

    remove comment main

--- _cmd/main.go
+++ _cmd/main.go
@@ -9,10 +9,7 @@ import (
 func main() {
 	fmt.Println("START GO ")
 	fmt.Println("Local TIME ZONE", time.Now())
-	//===========> START GO ===========<
-	fmt.Println("START----> SMS KUB SERVER")
+
 	app.StartServer()
-	fmt.Println("END----> SMS KUB SERVER")
-	//===========> END GO ===========<
 
 }
