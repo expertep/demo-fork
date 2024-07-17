2ec073196bf27caaf2b5e3bb1f052a7b2d77d4f4
expertep <esred@hotmail.com>
2024-07-17 19:54:00 +0700

    add api

--- route/route.go
+++ route/route.go
@@ -11,6 +11,10 @@ func Route(r *gin.Engine) {
 	guest := r.Group("/api")
 
 	guest.GET("/transaction/:id", controller.GetTransaction()) // get transaction by id
+	guest.GET("/transaction/:id/1", controller.GetTransaction())
+	guest.GET("/transaction/:id/3", controller.GetTransaction())
+	guest.GET("/transaction/:id/2", controller.GetTransaction())
+	guest.GET("/transaction/:id/4", controller.GetTransaction())
 
 	guest.POST("/transaction", controller.CreateTransaction())
 
